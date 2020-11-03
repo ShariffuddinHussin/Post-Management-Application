db61d0f8b69fcba00780d19ca586376143e62946
Shariffuddin-Hussin <shariffuddin.hussin@inx.my>
2020-11-03 17:21:59 +0800

    register controller

---
+++ app/Http/Controllers/PostCOntroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Post;
+use Illuminate\Http\Request;
+
+class PostCOntroller extends Controller
+{
+    public function index()
+    {
+        $post =
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::resource('products', 'PostController');
 });
